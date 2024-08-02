7fb122960b51749ee17137dc3935e234e561b476
omar-el-sayed <omarsayed281@gmail.com>
2024-08-02 21:35:47 +0300

    Add repository

---
+++ QuizSystem/Repository/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using QuizSystem.Data;
+using QuizSystem.Models;
+
+namespace QuizSystem.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QuizSystem/Repository/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizSystem.Repository
+{
+    public interface IGenericRepository<T>
+    {
+        IQueryable<T> GetAll();
+        T? GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
